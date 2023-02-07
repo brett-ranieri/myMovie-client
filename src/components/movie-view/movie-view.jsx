3ad5f1534a5d00a49dd2acdbf793fcb4f17c7f1f
Brett Ranieri <brett.ranieri@gmail.com>
2023-02-07 11:20:46 -0700

    add styling and formatting to movie-view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,42 +1,58 @@
+import { Col, Button } from "react-bootstrap";
+
 export const MovieView = ({ movie, onBackClick }) => { //need to destructure movie object, and onBackClick from main-view
     return (
-        <div>
+        <Col className="mt-5">
             <div>
-                <img className="w-100" src={movie.imagePath} alt="movie poster" />
+                <img className="w-100" mt={3} src={movie.imagePath} alt="movie poster" />
             </div>
-            <div>
-                <span>Title: </span>
-                <span>{movie.title}</span>
+            <br />
+            <div className="text-center">
+                <h1>{movie.title}</h1>
             </div>
+            <br />
             <div>
-                <span>Synopsis: </span>
+                <h5>Synopsis: </h5>
                 <span>{movie.description}</span>
             </div>
+            <br />
             <div>
-                <span>Director: </span>
+                <h5>Director: </h5>
                 <span>{movie.directorName}</span>
             </div>
+            <br />
             <div>
-                <span>Director Bio: </span>
+                <h5>Director Bio: </h5>
                 <span>{movie.directorBio}</span>
             </div>
+            <br />
             <div>
-                <span>Director Birth Year: </span>
+                <h5>Director Birth Year: </h5>
                 <span>{movie.directorBirth}</span>
             </div>
+            <br />
             <div>
-                <span>Director Death Year: </span>
+                <h5>Director Death Year: </h5>
                 <span>{movie.directorDeath}</span>
             </div>
+            <br />
             <div>
-                <span>Genre: </span>
+                <h5>Genre: </h5>
                 <span>{movie.genreName}</span>
             </div>
+            <br />
             <div>
-                <span>Genre Description: </span>
+                <h5>Genre Description: </h5>
                 <span>{movie.genreDescription}</span>
             </div>
-            <button onClick={onBackClick}>Back</button> 
-        </div> //button calls onBackClick function from main-view when clicked
+            <br />
+            <Button 
+                variant="primary"
+                className="mb-5"
+                onClick={onBackClick}
+            >
+                Back
+            </Button> 
+        </Col> //button calls onBackClick function from main-view when clicked
     );
 };
